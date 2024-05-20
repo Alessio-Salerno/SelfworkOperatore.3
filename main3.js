5050bd443d5626b2acbc24725d409b6d5e8283ea
Alessio-Salerno <156314493+Alessio-Salerno@users.noreply.github.com>
2024-05-20 11:56:43 +0200

    Add files via upload

---
+++ main3.js
@@ -0,0 +1,10 @@
+let Annocorrente = 2024;
+let Annodinascita = 1998;
+
+let age = Annocorrente - Annodinascita ;
+console.log(age);
+
+let mancanti = 100 - age ;
+console.log(mancanti); 
+
+console.log(`hai ${age} anni e ti mancano ${mancanti} per arrivare a 100`);
